6ff1de0fe5e1f24196226d55d13e788d94cb4a1a
fernando duenas <fernando39615@aim.com>
2020-09-28 14:49:36 -0500

    project

--- nodeimmersives.js
+++ nodeimmersives.js
@@ -1,5 +1,8 @@
 let firstName = process.argv[2];
 let lastName = process.argv[3];
+let email = process.argv[4]
 console.log('Good morning ' + firstName + ' ' + lastName);
 
 console.log(firstName.toUpperCase() + ' ' + lastName.toUpperCase() + " How are you? I am currently attempting to code.")
+console.log(firstName[0] + "." + lastName[0] + ".");
+console.log(firstName.toLowerCase() + "." + lastName.toLowerCase() + "@codeimmersives.com")
