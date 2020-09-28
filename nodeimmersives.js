0e00a2e572f93b337aaae90c7e351d8f3e9beff2
Deontre Hemingway <deontrehemingway@Deontres-MacBook-Pro.local>
2020-09-28 15:16:20 -0400

    Second commit

--- nodeimmersives.js
+++ nodeimmersives.js
@@ -2,4 +2,4 @@ let firstName = process.argv[2];
 let lastName = process.argv[3];
 console.log('Good morning ' + firstName + ' ' + lastName);
 
-// [0, 1, 2, 3]
+console.log(firstName.toUpperCase() + ' ' + lastName.toUpperCase() + " How are you? I am currently attempting to code.")
